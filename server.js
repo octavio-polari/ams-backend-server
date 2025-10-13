95d507dbe9552acaddcfee8acd76a3f99f55c940
tavyzin <polarioctavio@gmail.com>
2025-10-13 16:28:23 -0300

    improved error log

--- server.js
+++ server.js
@@ -55,7 +55,7 @@ app.post("/api/contact", (req, res) => {
     transporter.sendMail(mail, (error) => {
         if (error) {
             res.status(500).json({code: 500, error});
-            console.log(500,"Message Failed!")
+            console.log(500,"Message Failed!\n",error)
         } else {
             res.json({ code: 200, status: 'Message Sent!' })
             console.log(200,"Message Sent!")
